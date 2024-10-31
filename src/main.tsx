9b22aa45ff2c08c637fccb57860b6004bb64bef3
Mark <markbaltenko@gmail.com>
2024-10-31 22:23:44 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { StrictMode } from "react";
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
