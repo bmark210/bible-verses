0af298ec00d9676402aeb7616f3f677a9e80a288
Mark <markbaltenko@gmail.com>
2024-10-31 22:40:55 +0200

    fix styles

--- src/pages/verses/Verses.tsx
+++ src/pages/verses/Verses.tsx
@@ -17,41 +17,43 @@ const Verses = () => {
     //   document.removeEventListener("mousemove", handleMouseMove);
     // };
 
-    const interBubble = document.querySelector('.interactive') as HTMLElement;
+    const interBubble = document.querySelector(".interactive") as HTMLElement;
     let curX = 0;
     let curY = 0;
     let tgX = 0;
     let tgY = 0;
 
     function move() {
-        curX += (tgX - curX) / 20;
-        curY += (tgY - curY) / 20;
-        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
-        requestAnimationFrame(() => {
-            move();
-        });
+      curX += (tgX - curX) / 20;
+      curY += (tgY - curY) / 20;
+      interBubble.style.transform = `translate(${Math.round(
+        curX
+      )}px, ${Math.round(curY)}px)`;
+      requestAnimationFrame(() => {
+        move();
+      });
     }
 
-    window.addEventListener('mousemove', (event) => {
-        tgX = event.clientX;
-        tgY = event.clientY;
+    window.addEventListener("mousemove", (event) => {
+      tgX = event.clientX;
+      tgY = event.clientY;
     });
 
     move();
   }, []);
 
   // const handleMouseMove = () => {
-    // const element = e.target as HTMLElement;
-    // const targetElement = element.id.includes("verseCart") ? element : element.parentElement as HTMLElement;
-    // Check that targetElement is not null and contains element
-    // if (targetElement && targetElement.contains(element)) {
-    // const rect = element.getBoundingClientRect();
-    // const x = e.clientX - rect.left;
-    // const y = e.clientY - rect.top;
-    // element.style.setProperty("--x", x + "px");
-    // element.style.setProperty("--y", y + "px");
-    // const color = getRgbValues(element.style.background || '')[1];
-    // element.style.setProperty("--color", `rgb(${color.r}, ${color.g}, ${color.b})`);
+  // const element = e.target as HTMLElement;
+  // const targetElement = element.id.includes("verseCart") ? element : element.parentElement as HTMLElement;
+  // Check that targetElement is not null and contains element
+  // if (targetElement && targetElement.contains(element)) {
+  // const rect = element.getBoundingClientRect();
+  // const x = e.clientX - rect.left;
+  // const y = e.clientY - rect.top;
+  // element.style.setProperty("--x", x + "px");
+  // element.style.setProperty("--y", y + "px");
+  // const color = getRgbValues(element.style.background || '')[1];
+  // element.style.setProperty("--color", `rgb(${color.r}, ${color.g}, ${color.b})`);
   // };
   // };
 
@@ -66,26 +68,25 @@ const Verses = () => {
   return (
     <>
       <div className="gradient-bg">
-    <div className="main-container">
-    {verses.map((item, indexA) => (
-        <div className="block z-10" key={indexA}>
-          <div className="block z-10">
-            <h1 className="title text-black">{item.book}</h1>
-          </div>
-          <div className="carts-container">
-            {item.verses.map((verse, indexB) => (
-              <GlassCart
-                id={indexB}
-                key={indexB}
-                verse={verse}
-                author={item.book}
-              />
-            ))}
-          </div>
+        <div className="main-container">
+          {verses.map((item, indexA) => (
+            <div className="block z-10" key={indexA}>
+              <div className="block z-10">
+                <h1 className="title text-black">{item.book}</h1>
+              </div>
+              <div className="carts-container">
+                {item.verses.map((verse, indexB) => (
+                  <GlassCart
+                    id={indexB}
+                    key={indexB}
+                    verse={verse}
+                    author={item.book}
+                  />
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
-      
 
         <svg xmlns="http://www.w3.org/2000/svg">
           <defs>
@@ -113,12 +114,7 @@ const Verses = () => {
           <div className="g5"></div>
           <div className="interactive"></div>
         </div>
-
-      
-
       </div>
-
-      
     </>
   );
 };
